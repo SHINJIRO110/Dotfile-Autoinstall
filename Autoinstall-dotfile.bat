776f184264e9b1fb02d249331c335fa764c9ef8a
shinjiro <shinjiro397@gmail.com>
2025-05-26 02:05:01 -0300

    add .config

--- Autoinstall-dotfile.bat
+++ Autoinstall-dotfile.bat
@@ -1,10 +1,11 @@
 ::Instalando os programas
 winget install --id=Opera.OperaGX  -e
-winget install --id=LGUG2Z.komorebi  -e
+winget install --id=LGUG2Z.komorebi DEVCOM.JetBrainsMonoNerdFont 9WZDNCRDXF41 -e
 winget install --id=AmN.yasb  -e
 
 :: Criando os links sybolocos
-
+rmdir /s /q "%userprofile%\.config"
+mklink "%USERPROFILE%\.config" "%~dp0.config"
 mklink "%USERPROFILE%\komorebi.json" "%~dp0komorebi.json"
 mklink "%userprofile%\masir.ps1" "%~dp0masir.ps1"
 mklink "%userprofile%\masir.exe" "%~dp0masir.exe"
