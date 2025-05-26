1086c5778446e7c6d115ecf0d2f3f66076d3fb78
shinjiro <shinjiro397@gmail.com>
2025-05-26 03:35:03 -0300

    ta instalado?

--- Autoinstall-dotfile.bat
+++ Autoinstall-dotfile.bat
@@ -1,8 +1,23 @@
 ::Instalando os programas
-winget install --id=Opera.OperaGX  -e
-winget install --id=LGUG2Z.komorebi DEVCOM.JetBrainsMonoNerdFont 9WZDNCRDXF41 -e
-winget install --id=AmN.yasb  -e
+setlocal
 
+:: Função para instalar se o programa não estiver instalado
+call :install_if_missing "Opera GX" "Opera.OperaGX"
+call :install_if_missing "komorebi" "LGUG2Z.komorebi"
+call :install_if_missing "JetBrainsMono Nerd Font" "DEVCOM.JetBrainsMonoNerdFont"
+call :install_if_missing "Character Map UWP" "9WZDNCRDXF41"
+call :install_if_missing "yasb" "AmN.yasb"
+
+goto :eof
+
+:install_if_missing
+winget list --name "%~1" | findstr /I "%~1" >nul
+if errorlevel 1 (
+    echo Instalando %~1...
+    winget install --id=%~2 -e --accept-package-agreements --accept-source-agreements
+) else (
+    echo %~1 ja esta instalado. Pulando...
+)
 :: Criando os links sybolocos
 rmdir /s /q "%userprofile%\.config"
 mklink /d "%USERPROFILE%\.config" "%~dp0.config"
